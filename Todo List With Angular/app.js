13c59b7b5467b11303b5bce20176abbdec79a2d3
ayushchakraborty111 <ayushchakraborty.cse19@chitkarauniversity.edu.in>
2022-06-15 17:32:28 +0530

    first-commit

---
+++ Todo List With Angular/app.js
@@ -0,0 +1,61 @@
+let app = angular.module('myApp', []);
+
+app.controller('myCtrl', function($scope){
+    $scope.inp = "";
+    $scope.todos = [];
+    $scope.rem = 0;
+    $scope.editIndex = false;
+    $scope.addTodoFunc = function()
+    {
+        if($scope.inp.length != 0)
+        {
+            //object pushed into array
+            if($scope.editIndex === false)
+            {
+                //for filters
+                // let val = $scope.inp;
+                // let output = $filter('uppercase')(val);
+                $scope.todos.push({item: $scope.inp, completed: false});
+                $scope.rem = $scope.todos.length;
+            }
+            else
+            {
+                $scope.todos[$scope.editIndex].item = $scope.inp;
+            }
+            $scope.editIndex = false;
+            //$scope.show = false;
+            $scope.inp = "";
+        }
+        else
+        {
+            alert("Field shouldn't be empty");
+        }
+    }
+    $scope.countTask = function()
+    {
+        let count = 0;
+        // if($scope.todos.completed == true)
+        // {
+        //     count++;
+        // }
+        $scope.todos.forEach(todo=>{
+            if(todo.completed == true)
+            {
+                count++;
+            }
+        })        
+        return count;
+    }
+    $scope.deleteItem = function(index)
+    {
+        $scope.todos.splice(index, 1);
+        $scope.rem = $scope.todos.length;
+    }
+    $scope.editItem = function(index)
+    {
+        $scope.inp = $scope.todos[index].item;
+        $scope.editIndex = index;
+        //$scope.show = true;
+        //function appendAtIndex()
+    }
+})
